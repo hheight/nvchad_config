583aa99e2d713516bf423a4b3e2d10a6d5c1c9a4
Vitalii Shytikov <v.shytikov@gmail.com>
2024-08-14 20:07:53 +0200

    remove copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -78,25 +78,6 @@ return {
   },
 
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      }
-    end,
-  },
-
-  {
-    "zbirenbaum/copilot-cmp",
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-  },
-
-  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
