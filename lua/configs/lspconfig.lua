3ea8fac7695707a2c9ce2599036163192e6a14ab
Vitalii Shytikov <v.shytikov@gmail.com>
2024-09-06 15:46:37 +0200

    add copilot

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "tsserver", "clangd", "solargraph", "yamlls" }
+local servers = { "html", "cssls", "ts_ls", "clangd", "solargraph", "yamlls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
