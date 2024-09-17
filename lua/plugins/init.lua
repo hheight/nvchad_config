3f080cbb0963a6a2a72bf87a8bebec30cd676ef0
Vitalii Shytikov <v.shytikov@gmail.com>
2024-09-17 16:01:31 +0200

    copilot config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -108,37 +108,16 @@ return {
             open = "<M-CR>",
           },
           layout = {
-            position = "bottom", -- | top | left | right
-            ratio = 0.4,
+            position = "right", -- | top | left | right
           },
         },
         suggestion = {
           enabled = true,
-          auto_trigger = true,
-          hide_during_completion = true,
-          debounce = 75,
+          auto_trigger = false,
           keymap = {
             accept = "<tab>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
           },
         },
-        filetypes = {
-          yaml = false,
-          markdown = false,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          hgcommit = false,
-          svn = false,
-          cvs = false,
-          ["."] = false,
-        },
-        copilot_node_command = "node", -- Node.js version must be > 18.x
-        server_opts_overrides = {},
       }
     end,
   },
